cce13cb966b0f4b23b54d32984211d40c205577d
Bryden Fogelman <bryden1995@gmail.com>
2021-12-05 11:34:36 -0800

    cleaned up part1 solution

--- src/day_one.rs
+++ src/day_one.rs
@@ -2,25 +2,8 @@ use std::fs;
 
 pub fn find_depth() {
     let filename = "files/day_one_input.txt";
-    let mut contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
-    // remove new line at the end of the file
-    contents.truncate(contents.len() - 1);
-    // converts the string to an
-    let mut depths = contents.split("\n");
-    let mut count = 0;
-    // using `unwrap` like this is unsafe
-    //  next returns either `None` or `Some` if the value of the iterator exists
-    //  here I know it will exist but that's not always true
-    // TODO find a better pattern here
-    let mut prev: i32  = depths.next().unwrap().parse().unwrap();
-    for text in depths {
-        // this reads a string to an int
-        let num: i32 = text.parse().unwrap();
-        if num > prev {
-            count += 1;
-        }
-        prev = num;
-    }
-    println!("{}", count);
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file").trim().to_string();
+    let depths = contents.split("\n").map(|f| f.parse::<i32>().unwrap()).collect::<Vec<i32>>();
+    let sum: i32 = depths.iter().zip(depths.iter().skip(1)).map(|(a,b)| (b > a) as i32).sum();
+    println!("{}", sum);
 }
